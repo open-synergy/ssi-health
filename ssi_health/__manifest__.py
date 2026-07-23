a511f8ed5e9ceecebd008a4430bf1b8b76d535e7
Andhitia Rama <andhitia.r@gmail.com>
2026-07-23 18:29:46 +0700

    [ADD] ssi_health
    Modul base domain Health: menyediakan menu utama "Health",
    hierarki module category (Health + Workflow/Data Ownership/Configurator),
    dan submenu Configuration sebagai fondasi master data health.
    Belum memuat model maupun transaksi.

---
+++ ssi_health/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=C8101
+{
+    "name": "Health",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_master_data_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "menu.xml",
+    ],
+    "demo": [],
+    "contributors": [
+        "Andhitia Rama <andhitia.r@gmail.com>",
+    ],
+}
